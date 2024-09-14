28de6d9d571d0af8ff8506b521a46e842a0a268e
Mihai Bundea <mihaibundea1@gmail.com>
2024-09-14 15:34:36 +0300

    Added connection to exercises local database

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-# app.py
+import os
 from flask import Flask
 from routes.posts import posts_bp
 from routes.user_information import user_information_bp
 from routes.credentials import credentials_bp
 from Database.mongo_manager import MongoDBManager
 from AmazonS3.s3Manager import S3Manager
+from Database.local_mysql import LocalMySQL
+
+# Global flag to track initialization
+INITIALIZED = False
 
 def create_app():
+    global INITIALIZED
     app = Flask(__name__)
 
     @app.route('/', methods=['GET'])
@@ -14,11 +19,19 @@ def create_app():
         return "Successfully connected"
     
     create_route_blueprints(app)
-    create_databases(app)
-
-    app.s3_manager = S3Manager()  # Ensure you have defined S3Manager correctly
-
+    
+    if not INITIALIZED:
+        initialize_app(app)
+        INITIALIZED = True
+    
     return app
+
+def initialize_app(app):
+    print("Starting app initialization...")
+    create_databases(app)
+    create_local_mysql(app)
+    app.s3_manager = S3Manager()
+    print("App initialization complete.")
 
 def create_route_blueprints(app):
     app.register_blueprint(posts_bp, url_prefix='/posts')
@@ -35,7 +48,7 @@ def create_databases(app):
         username=mongodb_username,
         password=mongodb_password,
         cluster_url=mongodb_cluster_url,
-        database_name='user_data'  # Ensure this is correct
+        database_name='user_data'
     )
 
     mongo_content = MongoDBManager(
@@ -50,6 +63,25 @@ def create_databases(app):
     
     app.user_data = mongo_user_data.get_database()
     app.db_content = mongo_content.get_database()
+
+def create_local_mysql(app):
+    app.mysql_db = LocalMySQL()
+    app.mysql_db.connect()
+    
+    if app.mysql_db.cursor:
+        tables = app.mysql_db.get_table_names()
+        if tables:
+            print(f"Connected to MySQL. Tables: {tables}")
+        else:
+            print("Connected to MySQL. No tables found.")
+    else:
+        print("Failed to connect to MySQL database.")
+
+    @app.teardown_appcontext
+    def close_mysql_connection(exception=None):
+        mysql_db = getattr(app, 'mysql_db', None)
+        if mysql_db:
+            mysql_db.disconnect()
 
 if __name__ == '__main__':
     app = create_app()
